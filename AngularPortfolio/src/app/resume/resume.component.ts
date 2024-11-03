d78dd844f5ce7ec09a48c6a629cf5f8f95fb8c49
jybuissereth <jbuissereth@outlook.fr>
2024-11-03 19:36:47 +0100

    Creation page contact

--- AngularPortfolio/src/app/resume/resume.component.ts
+++ AngularPortfolio/src/app/resume/resume.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion'; 
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule,NgIf],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css',
 })
 export class ResumeComponent {
-  constructor(private titleService: Title) {
+  isExperienceOpen:boolean=false;
+  isEducationOpen:boolean=false;
+  isDiplomeOpen:boolean=false;
+  isCompetenceOpen:boolean=false;
+
+  constructor(private titleService: Title,private renderer:Renderer2) {
     this.titleService.setTitle('Jean-Yves - Resume');
+  }
+
+  DownloadFile(){
+    const link = this.renderer.createElement("a");
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/CV.pdf');
+    link.setAttribute('download', 'CV.pdf');
+    link.click();
+    link.remove();
+
   }
 }
